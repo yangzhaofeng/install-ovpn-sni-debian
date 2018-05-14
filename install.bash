c3d85e3340f702da30fed8d8e790b0ada6bbbb7b
Steven Yang <yangzhaofengsteven@gmail.com>
2018-05-14 10:45:17 +0800

    dockerize

--- install.bash
+++ install.bash
@@ -19,7 +19,7 @@ add-apt-repository \
 	$(lsb_release -cs) \
 	stable"
 apt-get update
-apt-get install docker-ce
+apt-get install -y docker-ce
 mkdir /etc/docker
 touch /etc/docker/daemon.json
 echo '{' >> /etc/docker/daemon.json
@@ -45,37 +45,54 @@ docker run -itd \
 	gaoyifan/sniproxy
 
 if [ `dpkg -l | grep bind9 |wc -l` -ne 1 ];then
-apt-get install bind9
 ip a add 192.168.142.254 dev lo
-mv /etc/bind/named.conf.options /etc/bind/named.conf.options.old
-mv /etc/bind/named.conf.local /etc/bind/named.conf.local.old
-cp named.conf.options named.conf.local db.rpz /etc/bind/
-echo '*.magi-reco.com		A	192.168.142.1' >> /etc/bind/db.rpz
-systemctl start bind9.service
+git clone https://github.com/yangzhaofeng/bind-magireco.git /srv/docker/bind
+cp named.conf.options /srv/docker/bind/
+docker run -itd \
+	--restart=always \
+	--name=bind9 \
+	--net=host \
+	--privileged \
+	-v /srv/docker/bind:/etc/bind \
+	yangzhaofengsteven/bind9
+fi
+
+if [ `dpkg -l | grep easy-rsa |wc -l` -ne 1 ];then
+	apt-get install -y easy-rsa
 fi
 
 if [ `dpkg -l | grep openvpn |wc -l` -ne 1 ];then
-apt-get install openvpn easy-rsa
-mkdir /etc/openvpn/easy-rsa/
-cp -r /usr/share/easy-rsa/* /etc/openvpn/easy-rsa/
-mv /etc/openvpn/easy-rsa/vars /etc/openvpn/easy-rsa/vars.old
-cp vars /etc/openvpn/easy-rsa/vars
-source /etc/openvpn/easy-rsa/vars
-/etc/openvpn/easy-rsa/clean-all
-/etc/openvpn/easy-rsa/build-ca
-/etc/openvpn/easy-rsa/build-key-server server
-/etc/openvpn/easy-rsa/build-dh
-cp /etc/openvpn/easy-rsa/keys/server.crt /etc/openvpn/
-cp /etc/openvpn/easy-rsa/keys/server.key /etc/openvpn/
-cp /etc/openvpn/easy-rsa/keys/ca.crt /etc/openvpn/
-cp /etc/openvpn/easy-rsa/keys/dh2048.pem /etc/openvpn/
-source /etc/openvpn/easy-rsa/vars
-/etc/openvpn/easy-rsa/build-key client1
-mv /etc/openvpn/server.conf /etc/openvpn/server.conf.old
-cp server.conf /etc/openvpn/
+mkdir /srv/docker/openvpn/
+cp -r /usr/share/easy-rsa/ /srv/docker/openvpn/
+mv /srv/docker/openvpn/easy-rsa/vars /srv/docker/openvpn/easy-rsa/vars.old
+cp vars /srv/docker/openvpn/easy-rsa/vars
+source /srv/docker/openvpn/easy-rsa/vars
+/srv/docker/openvpn/easy-rsa/clean-all
+/srv/docker/openvpn/easy-rsa/build-ca
+/srv/docker/openvpn/easy-rsa/build-key-server server
+/srv/docker/openvpn/easy-rsa/build-dh
+cp /srv/docker/openvpn/easy-rsa/keys/server.crt /srv/docker/openvpn/
+cp /srv/docker/openvpn/easy-rsa/keys/server.key /srv/docker/openvpn/
+cp /srv/docker/openvpn/easy-rsa/keys/ca.crt /srv/docker/openvpn/
+cp /srv/docker/openvpn/easy-rsa/keys/dh2048.pem /srv/docker/openvpn/
+source /srv/docker/openvpn/easy-rsa/vars
+/srv/docker/openvpn/easy-rsa/build-key client1
+#mv /srv/docker/openvpn/server.conf /srv/docker/openvpn/server.conf.old
+cp server.conf /srv/docker/openvpn/
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 sysctl -p
-systemctl start openvpn@server.service
+
+docker run -itd \
+	-v /srv/docker/openvpn/:/etc/openvpn/:ro \
+	-v /var/log/openvpn:/var/log/openvpn \
+	--net=host \
+	--restart=always \
+	--privileged \
+	--name=openvpn \
+	--sysctl net.ipv6.conf.all.disable_ipv6=0 \
+	yangzhaofengsteven/docker-openvpn \
+	/usr/sbin/openvpn --cd /etc/openvpn/ --config server.conf
+
 touch client1.ovpn
 echo 'client' >> client1.ovpn
 echo 'proto udp' >> client1.ovpn
@@ -86,13 +103,13 @@ echo 'persist-tun' >> client1.ovpn
 echo 'dev myvpn' >> client1.ovpn
 echo 'dev-type tun' >> client1.ovpn
 echo '<ca>' >> client1.ovpn
-cat /etc/openvpn/ca.crt >> client1.ovpn
+cat /srv/docker/openvpn/ca.crt >> client1.ovpn
 echo '</ca>' >> client1.ovpn
 echo '<cert>' >> client1.ovpn
-cat /etc/openvpn/easy-rsa/keys/client1.crt >> client1.ovpn
+cat /srv/docker/openvpn/easy-rsa/keys/client1.crt >> client1.ovpn
 echo '</cert>' >> client1.ovpn
 echo '<key>' >> client1.ovpn
-cat /etc/openvpn/easy-rsa/keys/client1.key >> client1.ovpn
+cat /srv/docker/openvpn/easy-rsa/keys/client1.key >> client1.ovpn
 echo '</key>' >> client1.ovpn
 
 fi
